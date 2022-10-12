f673695784028b2aaa0d83bf3d25aef30d7f5ee9
Marius Andra <marius.andra@gmail.com>
2022-10-13 01:08:28 +0200

    capture elements

--- src/server.mjs
+++ src/server.mjs
@@ -85,11 +85,28 @@ export function startServer(opts = {}) {
                 <head>
                     <title>${pluginJson.name || 'PostHog App'}</title>
                     <script>
+                        function eventToElement(event, properties) {
+                            return Object.assign(document.createElement('pre'), { 
+                                innerHTML: JSON.stringify({ event, properties }, null, 2) 
+                            })
+                        }
+                        function addElement(event, properties) {
+                            document.getElementById('events').appendChild(eventToElement(event, properties))
+                        }
                         !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
                         posthog.init(${JSON.stringify(posthogKey)},{
                             api_host: ${JSON.stringify(posthogHost)},
-                            opt_in_site_apps: false, // opt out of all other apps 
+                            opt_in_site_apps: false, // opt out of all other apps
+                            verbose: true, 
+                            _onCapture: (event, properties) => {
+                                addElement(event, properties)
+                            }
                         })
+                        var capture = posthog.capture
+                        posthog.capture = (event, properties) => {
+                            capture(event, properties)
+                            addElement(event, properties)
+                        } 
                     </script>
                     <script type="text/javascript">
                         if ('EventSource' in window) {
@@ -119,6 +136,8 @@ export function startServer(opts = {}) {
                 <body>
                     <h1>${pluginJson.name || 'PostHog Site App'}</h1>
                     <pre>plugin.json = ${JSON.stringify(pluginJson, null, 2)}</pre>
+                    <h2>Captured Events</h2>
+                    <div id='events'></div>
                     <h2>site.ts</h2>
                     ${
                         siteJs
